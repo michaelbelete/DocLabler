40b45a754b25a9748ca7ad3773efbd8d5a35e78b
Michael Belete <it.michael.belete@gmail.com>
2023-08-26 17:37:15 +0300

    chore: change favicon and meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,14 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<title>DocLabeler - Easy-to-use solution for NLP-driven document labeling</title>
+	<meta name="title" content="DocLabeler - Easy-to-use solution for NLP-driven document labeling" />
+	<meta
+		name="description"
+		content="Elevate your document labeling with DocLabler's user-friendly interface. Experience the power of NLP-driven tagging and organization on a simple, responsive platform."
+	/>
+	<meta name="author" content="Michael Belete" />
+</svelte:head>
+
 <slot />
