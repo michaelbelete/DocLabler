e95a7cda5c04451e79f70b97b8a4963780f2b99a
Michael Belete <it.michael.belete@gmail.com>
2023-08-27 05:03:51 +0300

    chore: add tailwind form

--- src/routes/documents/[id]/+page.ts
+++ src/routes/documents/[id]/+page.ts
@@ -1,12 +1,15 @@
 import { error } from '@sveltejs/kit';
+import { documents } from '$lib/mock/document';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = ({ params }) => {
-	if (params.id) {
+export const load: PageLoad = async ({ params }) => {
+	const document = documents.find((d) => d.id === parseInt(params.id));
+
+	if (document) {
 		return {
-			id: params.id
+			document
 		};
 	}
 
-	throw error(404, 'Not found');
+	throw error(404, 'Document Not found');
 };
