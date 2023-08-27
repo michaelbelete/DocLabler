2f263ec8cfeabb4a1c211061f0ad06ffecfa5510
Michael Belete <it.michael.belete@gmail.com>
2023-08-27 05:39:23 +0300

    feat: Homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,133 @@
-<a href="/documents">Get Started</a>
+<div class="bg-gray-50">
+	<div class="relative overflow-hidden">
+		<div class="absolute inset-y-0 h-full w-full" aria-hidden="true">
+			<div class="relative h-full">
+				<svg
+					class="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
+					width="404"
+					height="784"
+					fill="none"
+					viewBox="0 0 404 784"
+				>
+					<defs>
+						<pattern
+							id="e229dbec-10e9-49ee-8ec3-0286ca089edf"
+							x="0"
+							y="0"
+							width="20"
+							height="20"
+							patternUnits="userSpaceOnUse"
+						>
+							<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+						</pattern>
+					</defs>
+					<rect width="404" height="784" fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)" />
+				</svg>
+				<svg
+					class="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+					width="404"
+					height="784"
+					fill="none"
+					viewBox="0 0 404 784"
+				>
+					<defs>
+						<pattern
+							id="d2a68204-c383-44b1-b99f-42ccff4e5365"
+							x="0"
+							y="0"
+							width="20"
+							height="20"
+							patternUnits="userSpaceOnUse"
+						>
+							<rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+						</pattern>
+					</defs>
+					<rect width="404" height="784" fill="url(#d2a68204-c383-44b1-b99f-42ccff4e5365)" />
+				</svg>
+			</div>
+		</div>
+
+		<div class="relative pt-6 pb-16 sm:pb-24">
+			<div>
+				<div class="max-w-7xl mx-auto px-4 sm:px-6">
+					<nav
+						class="relative flex items-center justify-between sm:h-10 md:justify-center"
+						aria-label="Global"
+					>
+						<div class="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+							<div class="flex items-center justify-between w-full md:w-auto">
+								<a href="/">
+									<span class="sr-only">DocLabeler</span>
+									<img class="h-8 w-auto sm:h-12" src="/images/logo.svg" alt="DocLabeler Logo" />
+								</a>
+								<div class="-mr-2 flex items-center md:hidden">
+									<button
+										type="button"
+										class="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary"
+										aria-expanded="false"
+									>
+										<span class="sr-only">Open main menu</span>
+										<!-- Heroicon name: outline/menu -->
+										<svg
+											class="h-6 w-6"
+											xmlns="http://www.w3.org/2000/svg"
+											fill="none"
+											viewBox="0 0 24 24"
+											stroke="currentColor"
+											aria-hidden="true"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M4 6h16M4 12h16M4 18h16"
+											/>
+										</svg>
+									</button>
+								</div>
+							</div>
+						</div>
+						<div class="absolute flex items-center justify-end inset-y-0 right-0">
+							<span class="inline-flex rounded-md shadow">
+								<a href="/documents" class="btn-primary"> Start Labeling </a>
+							</span>
+						</div>
+					</nav>
+				</div>
+			</div>
+
+			<div class="mt-16 mx-auto max-w-7xl px-4 sm:mt-24 sm:px-6">
+				<div class="flex flex-col text-center">
+					<h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+						<span class="block">Unlock insights with</span>
+						<span class="block text-primary">smart labeling</span>
+					</h1>
+					<p
+						class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl"
+					>
+						Join the Effort to Train AI Models Through Intuitive Document Labeling This version
+						focuses on the collaborative nature of the app and how users can contribute to
+					</p>
+					<div class="pt-6">
+						<a href="/documents" class="btn-primary !text-lg !py-3"> Start Labeling </a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="relative">
+			<div class="absolute inset-0 flex flex-col" aria-hidden="true">
+				<div class="flex-1" />
+				<div class="flex-1 w-full bg-secondary" />
+			</div>
+			<div class="max-w-7xl mx-auto px-4 sm:px-6">
+				<img
+					class="relative rounded-lg shadow-lg"
+					src="/images/screen-shot.png"
+					alt="App screenshot"
+				/>
+			</div>
+		</div>
+	</div>
+	<div class="bg-secondary py-16" />
+</div>
